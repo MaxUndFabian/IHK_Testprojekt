7a5684d9849788c374c3adafe8926df6ed9c6b6b
Fab432 <Fabian@Fabians-MacBook-Pro.local>
2016-02-09 16:44:47 +0100

    updated

--- src/client/ts/main.ts
+++ src/client/ts/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}  from 'angular2/platform/browser';
 import {AppRoutingComponent} from './app-routing.component';
+import {HTTP_PROVIDERS} from 'angular2/http';
 
-bootstrap(AppRoutingComponent);
+bootstrap(AppRoutingComponent, [HTTP_PROVIDERS]);
