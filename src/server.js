136d0f3e6d7c890562e13b6b9a70b5d39ca887de
Max Dierkes <max@Maxs-MacBook-Pro.local>
2016-02-09 15:34:34 +0100

    added basic api for news

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 var express = require('express');
+var newsController = require('./server/controllers/news_controller');
 var app = express();
 
 app.use('/client', express.static('client'));
 app.use('/app', express.static('client/ts'));
 
+// 
+app.post('/api/news', newsController.create);
+app.put('/api/news/:id', newsController.update)
+app.get('/api/news', newsController.list);
+app.get('/api/news/:id', newsController.single);
+app.delete('/api/news/:id', newsController.delete);
+
 
 app.use(function(req, res){
    res.sendFile(__dirname + "/client/index.html"); 
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
