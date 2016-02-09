a425ed077b512d04ad1fe7e7334c1f9c0b7677db
Max Dierkes <max@Maxs-MacBook-Pro.local>
2016-02-09 22:55:57 +0100

    http for news now working

--- src/client/ts/main.ts
+++ src/client/ts/main.ts
@@ -1,5 +1,6 @@
 import {bootstrap}  from 'angular2/platform/browser';
 import {AppRoutingComponent} from './app-routing.component';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/Rx';
 
 bootstrap(AppRoutingComponent, [HTTP_PROVIDERS]);
--- src/client/ts/news-interface.ts
+++ src/client/ts/news-interface.ts
@@ -1,6 +1,12 @@
-export interface News {
-  id: number;
-  title: string;
-  content: string;
-  date: Date;
+export class News {
+  constructor(id: number, 
+              title: string, 
+              content: string, 
+              creationDate: Date, 
+              lastModifiedDate: Date, 
+              username: string, 
+              tag_name: string)
+  {
+      
+  }
 }
