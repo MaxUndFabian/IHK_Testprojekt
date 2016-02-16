31642199a17f03dc6e7b35c0a047976b6e9a9465
Fab432 <Fabian@Fabians-MacBook-Pro.local>
2016-02-16 15:21:45 +0100

    Design changes + new Sidebar

--- src/client/ts/news-sidebar.component.ts
+++ src/client/ts/news-sidebar.component.ts
@@ -1,27 +1,43 @@
 import {Component} from 'angular2/core';
 import {News} from './news.interface';
 import {NewsService} from './news.service';
+import { DateSortPipe } from './date_sort.pipe';
+import {Router} from 'angular2/router';
 
 @Component({
   selector: 'sidebar',
-  template:`
-    <h2>News</h2>
-    <ul class="news">
-      <li *ngFor="#news of news">
-        <span class="badge">{{news.id}}</span> {{news.title}}
-      </li>
-    </ul>
-  `,
-  providers: [NewsService]
+  templateUrl: 'client/html/sidebar.html',
+  providers: [NewsService],
+  pipes: [DateSortPipe]
 })
-export class AppComponent implements OnInit {
+export class SidebarComponent {
   news: News[];
   News: News;
-  constructor(private _newsService: NewsService) { }
+  constructor(private _router: Router, private _newsService: NewsService) { }
   getNews() {
-    this._newsService.getNews().then(news => this.news = news);
+    this._newsService.getNews().subscribe((news) => {
+        this.news = news.slice(news.length-2,news.length);
+        this.news.forEach(elem => {
+            elem.creationDate = new Date(elem.creationDate);
+            elem.content = this.contentPreview(elem.content);
+        });
+    });
   }
   ngOnInit() {
     this.getNews();
+  }
+  gotoArticle(id: number){
+      let link = ['NewsArticle', {id: id}];
+      this._router.navigate(link);
+  }
+  contentPreview(content: string){
+      content = content.substring(0, 250);
+      if(content.lastIndexOf(' ') != -1 && content.length == 250){
+        content = content.substring(0, content.lastIndexOf(' '));
+        if(!(content.length < 200)){
+          content += '....';
+        }
+      }
+      return content;
   }
 }
