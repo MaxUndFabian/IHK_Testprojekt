21d72da033844c8a60c0c1c799cf9961699e192f
Max Dierkes <max@Maxs-MacBook-Pro.local>
2016-02-23 16:47:42 +0100

    registering users should now work

--- src/client/ts/register.component.ts
+++ src/client/ts/register.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {NgForm, FORM_DIRECTIVES, Control, ControlGroup, Validators, FormBuilder} from 'angular2/common';
+import {Http, Response, Headers, RequestOptions}    from 'angular2/http';
 
 @Component({
   // Declare the tag name in index.html to where the component attaches
@@ -18,7 +19,7 @@ export class RegisterComponent {
     pw_confirm: Control;
     hinderLoop: boolean;
         
-    constructor(private builder: FormBuilder){
+    constructor(private builder: FormBuilder, private http: Http){
         this.username = new Control("", Validators.compose([Validators.required]));
         this.firstname = new Control("", Validators.compose([Validators.required]));
         this.lastname = new Control("", Validators.compose([Validators.required]));
@@ -69,14 +70,36 @@ export class RegisterComponent {
     
     
     registerUser(){
-        console.log('register');
         
+        console.log('register');
+        var username = this.input_username;
+        var password = this.input_password;
+        var firstname = this.input_firstname;
+        var lastname = this.input_lastname;
+        var email = "test@maxisanidiot.com";
+        
+        
+        let body = JSON.stringify({username, password, firstname, lastname, email});
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+      
+        this.http.post('api/register', body, options)
+                        .map(res => <News> res.json())
+                        .catch(this.handleError)
+                        .subscribe((res) => {
+                            
+                        });
         this.input_firstname = '';
         this.input_lastname = '';
         this.input_password = '';
         this.input_pw_confirm = '';
         this.input_username = '';
-        
-        return false;
+    }
+    
+    private handleError (error: Response) {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        console.error(error);
+        return Observable.throw(error || 'Server error');
     }
 }
